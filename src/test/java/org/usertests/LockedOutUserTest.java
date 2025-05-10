0b98bbbf6e6e1266b596105a60783f281588cb6b
mihai <stoenescumihai88@gmail.com>
2025-05-10 22:20:01 +0300

    Adaugarea a 2 teste pentru lockedOutUser- Logare fara a introduce o parola si logare cu o parola random. Schimbare in BaseTest ca fiecare fereastra sa se inchida dupa rulare

--- src/test/java/org/usertests/LockedOutUserTest.java
+++ src/test/java/org/usertests/LockedOutUserTest.java
@@ -1,5 +1,6 @@
 package org.usertests;
 
+import com.github.javafaker.Faker;
 import org.BaseTest;
 import org.pageobject.LoginPage;
 import org.testng.Assert;
@@ -8,10 +9,13 @@ import org.testng.annotations.Test;
 
 public class LockedOutUserTest extends BaseTest {
     LoginPage loginPage;
+    Faker faker;
 
     @BeforeMethod
     public void setUpPages() {
         loginPage = new LoginPage(driver);
+        faker = new Faker();
+
     }
 
     @Test
@@ -22,5 +26,23 @@ public class LockedOutUserTest extends BaseTest {
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/");
         Assert.assertEquals(loginPage.getErrorMessage(), "Epic sadface: Sorry, this user has been locked out.");
 
+    }
+
+    @Test
+    public void lockedOutUserNoPasswordLogin() {
+        loginPage.fillUsernameField("locked_out_user");
+        loginPage.clickOnLoginButton();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/");
+        Assert.assertEquals(loginPage.getErrorMessage(), "Epic sadface: Password is required");
+    }
+
+    @Test
+    public void lockedOutUserRandomPasswordLogin() {
+        loginPage.fillUsernameField("locked_out_user");
+        String password = faker.internet().password();
+        loginPage.fillPasswordField(password);
+        loginPage.clickOnLoginButton();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/");
+        Assert.assertEquals(loginPage.getErrorMessage(), "Epic sadface: Username and password do not match any user in this service");
     }
 }
