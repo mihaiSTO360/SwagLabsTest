30a32322c9dc85665ce7aa7419128a44ad575430
mihai <stoenescumihai88@gmail.com>
2025-05-16 23:04:11 +0300

    Teste din standard user adaptate pentru error user

--- src/test/java/org/usertests/ErrorUserUserTest.java
+++ src/test/java/org/usertests/ErrorUserUserTest.java
@@ -1,17 +1,25 @@
 package org.usertests;
 
 import org.BaseTest;
+import org.pageobject.CheckoutPage;
 import org.pageobject.LoginPage;
+import org.pageobject.MainPage;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Set;
+
 public class ErrorUserUserTest extends BaseTest {
     LoginPage loginPage;
+    MainPage mainPage;
+    CheckoutPage checkoutPage;
 
     @BeforeMethod
     public void setUpPages() {
         loginPage = new LoginPage(driver);
+        mainPage = new MainPage(driver);
+        checkoutPage = new CheckoutPage(driver);
     }
 
     @Test
@@ -26,6 +34,100 @@ public class ErrorUserUserTest extends BaseTest {
         loginPage.clickOnLoginButton();
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/");
         Assert.assertEquals(loginPage.getErrorMessage(), "Epic sadface: Password is required");
+
+    }
+
+    @Test
+    public void socialMediaTwitterButtonTest() {
+        loginPage.errorLogin();
+        String originalWindow = driver.getWindowHandle();
+        mainPage.clickOnTwitterButton();
+        Set<String> windowHandles = driver.getWindowHandles();
+        for (String windowHandle : windowHandles) {
+            if (!windowHandle.equals(originalWindow)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+        Assert.assertEquals(driver.getCurrentUrl(), "https://x.com/saucelabs");
+    }
+
+    @Test
+    public void socialMediaFacebookButtonTest() {
+        loginPage.errorLogin();
+        String originalWindow = driver.getWindowHandle();
+        mainPage.clickOnFacebookButtonSelector();
+        Set<String> windowHandles = driver.getWindowHandles();
+        for (String windowHandle : windowHandles) {
+            if (!windowHandle.equals(originalWindow)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/saucelabs");
+    }
+
+    @Test
+    public void socialMediaLinkedinButtonTest() {
+        loginPage.errorLogin();
+        String originalWindow = driver.getWindowHandle();
+        mainPage.clickOnLinkedinButtonSelector();
+        Set<String> windowHandles = driver.getWindowHandles();
+        for (String windowHandle : windowHandles) {
+            if (!windowHandle.equals(originalWindow)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/company/sauce-labs/");
+    }
+
+    @Test
+    public void aboutPageTest() {
+        loginPage.errorLogin();
+        mainPage.clickOnBurgerMenuButton();
+        mainPage.clickOnAboutPage();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://saucelabs.com/");
+
+    }
+
+    @Test
+    public void logOutTest() {
+        loginPage.errorLogin();
+        mainPage.clickOnBurgerMenuButton();
+        mainPage.clickOnLogoutPage();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/");
+
+    }
+
+    @Test
+    public void allItemsButtonTest() {
+        loginPage.errorLogin();
+        mainPage.openBackpackItemPage();
+        mainPage.clickOnBurgerMenuButton();
+        mainPage.clickOnAllItemsPage();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html");
+
+    }
+
+    @Test
+    public void resetAppStateTest() {
+        loginPage.errorLogin();
+        mainPage.addBackpackToCart();
+        mainPage.clickOnBurgerMenuButton();
+        mainPage.clickOnResetAppState();
+        mainPage.clickOnShoppingCartSelector();
+        checkoutPage.getCheckoutItemsNumber();
+        Assert.assertEquals(checkoutPage.checkoutItemCount, 0);
+    }
+
+    @Test
+    public void continueShoppingButtonTest() {
+        loginPage.errorLogin();
+        mainPage.addBackpackToCart();
+        mainPage.clickOnShoppingCartSelector();
+        checkoutPage.clickOnContinueShoppingButton();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html");
 
     }
 
